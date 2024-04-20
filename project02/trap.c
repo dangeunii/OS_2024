d391492dac1c0f620ef1ddcf6c6e843129aefaa1
고다은 <2021057301>
2024-04-20 17:50:14 +0000

    project02

---
+++ project02/trap.c
@@ -0,0 +1,162 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
+#include "traps.h"
+#include "spinlock.h"
+
+// Interrupt descriptor table (shared by all CPUs).
+struct gatedesc idt[256];
+extern uint vectors[];  // in vectors.S: array of 256 entry pointers
+struct spinlock tickslock;
+uint ticks;
+
+void
+tvinit(void)
+{
+  int i;
+
+  for(i = 0; i < 256; i++)
+    SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
+  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
+
+  initlock(&tickslock, "time");
+}
+
+void
+idtinit(void)
+{
+  lidt(idt, sizeof(idt));
+}
+
+//PAGEBREAK: 41
+void
+trap(struct trapframe *tf)
+{
+  int boosting = 0;
+  if(tf->trapno == T_SYSCALL){
+    if(myproc()->killed)
+      exit();
+    myproc()->tf = tf;
+    syscall();
+    if(myproc()->killed)
+      exit();
+    return;
+  }
+
+  switch(tf->trapno){
+  case T_IRQ0 + IRQ_TIMER:
+    if(cpuid() == 0){
+      // global ticks 1 증가
+      // 반드시 ticks 변수를 건드리기 전에는 lock을 이용해서 동기화 문제를 해결해야 한다.
+      acquire(&tickslock);
+      ticks++;
+
+      if(ticks == 100){
+        ticks = 0;
+        boosting = 1;
+      }
+      wakeup(&ticks);
+      release(&tickslock);
+
+      if(boosting == 1){
+        priority_boosting();
+        boosting = 0;
+      }
+      else{
+        struct proc *p = myproc();
+        // p가 RUNNING 상태일 때
+        if(p && p->state == RUNNING) {
+          if(p->monopolize == 1){
+            p->tick = 0;
+          }
+          else{
+            p -> tick++;
+            // p가 그 다음 Queue로 넘어가는 경우
+            if(p->tick >= 2 * (p->level) + 2){
+              if(p -> level == 0){
+                // L0에서는 pid가 홀수면 L1로, 짝수면 L2로 보낸다.
+                if((p->pid) %2 == 0){
+                  p->level = 1;
+                  p->tick = 0;
+                }
+                else{
+                  p->level = 2;
+                  p->tick = 0;
+                }
+              }
+              else if( p->level == 1 || p->level == 2){
+                p->level = 3;
+                p->tick = 0;
+              }
+              else if( p->level == 3){
+                if(p->priority != 0){
+                  p->priority--;
+                }
+                p->tick = 0;
+              }
+            }
+          }
+        }
+        
+      }
+    }
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_IDE:
+    ideintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_IDE+1:
+    // Bochs generates spurious IDE1 interrupts.
+    break;
+  case T_IRQ0 + IRQ_KBD:
+    kbdintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + IRQ_COM1:
+    uartintr();
+    lapiceoi();
+    break;
+  case T_IRQ0 + 7:
+  case T_IRQ0 + IRQ_SPURIOUS:
+    cprintf("cpu%d: spurious interrupt at %x:%x\n",
+            cpuid(), tf->cs, tf->eip);
+    lapiceoi();
+    break;
+
+  //PAGEBREAK: 13
+  default:
+    if(myproc() == 0 || (tf->cs&3) == 0){
+      // In kernel, it must be our mistake.
+      cprintf("unexpected trap %d from cpu %d eip %x (cr2=0x%x)\n",
+              tf->trapno, cpuid(), tf->eip, rcr2());
+      panic("trap");
+    }
+    // In user space, assume process misbehaved.
+    cprintf("pid %d %s: trap %d err %d on cpu %d "
+            "eip 0x%x addr 0x%x--kill proc\n",
+            myproc()->pid, myproc()->name, tf->trapno,
+            tf->err, cpuid(), tf->eip, rcr2());
+    myproc()->killed = 1;
+  }
+
+  // Force process exit if it has been killed and is in user space.
+  // (If it is still executing in the kernel, let it keep running
+  // until it gets to the regular system call return.)
+  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
+    exit();
+
+  // Force process to give up CPU on clock tick.
+  // If interrupts were on while locks held, would need to check nlock.
+  if(myproc() && myproc()->state == RUNNING &&
+     tf->trapno == T_IRQ0+IRQ_TIMER)
+    yield();
+
+  // Check if the process has been killed since we yielded
+  if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
+    exit();
+}
